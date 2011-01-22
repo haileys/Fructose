744e637b05dd94a6f431223da73230ae32fe9469
Charlie Somerville <charlie@charliesomerville.com>
2011-01-22 17:58:27 +1100

    testing

---
+++ Fructose.Test/Tests/HelloWorld.rb
@@ -0,0 +1,14 @@
+﻿#TEST EXPECTS:
+#Hello, World!
+
+class Greeter
+  def initialize(greeting)
+    @greeting = greeting
+  end
+
+  def greet!
+    puts @greeting
+  end
+end
+
+Greeter.new("Hello, World!").greet!
