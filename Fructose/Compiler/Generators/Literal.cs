ca45d46014d6ca3e81f4c9c222021476d8bf5cef
Charlie Somerville <charlie@charliesomerville.com>
2011-01-23 00:14:49 +1100

    woops forgot to add some files

---
+++ Fructose/Compiler/Generators/Literal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronRuby.Compiler.Ast;
+
+namespace Fructose.Compiler.Generators
+{
+    [Generator(NodeTypes.Literal)]
+    public class LiteralGenerator : AstNodeGenerator
+    {
+        public override void Compile(Compiler compiler, Node node, NodeParent parent)
+        {
+            compiler.AppendLine("$_stack[] = F_Number::__from_number({0});", ((Literal)node).Value.ToString());
+        }
+    }
+}
---
+++ Fructose/Compiler/Generators/StringSymbolConstructor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronRuby.Compiler.Ast;
+
+namespace Fructose.Compiler.Generators
+{
+    [Generator(NodeTypes.SymbolLiteral)]
+    public class SymbolLiteralGenerator : AstNodeGenerator
+    {
+        public override void Compile(Compiler compiler, Node node, NodeParent parent)
+        {
+            compiler.AppendLine("$_stack[] = F_Symbol::__from_string('{0}');", ((SymbolLiteral)node).Value.ToString());
+        }
+    }
+
+    [Generator(NodeTypes.StringConstructor)]
+    public class StringConstructorGenerator : AstNodeGenerator
+    {
+        public override void Compile(Compiler compiler, Node node, NodeParent parent)
+        {
+            var sc = (StringConstructor)node;
+
+            switch (sc.Kind)
+            {
+                case StringKind.Symbol:
+                    if (sc.Parts.Count != 1 || sc.Parts[0].NodeType != NodeTypes.StringLiteral)
+                        throw new FructoseCompileException("Symbol interpolation not supported _yet_", node);
+                    compiler.AppendLine("$_stack[] = F_Symbol::__from_string('{0}');", ((StringLiteral)sc.Parts[0]).Value.ToString().Replace("'", "\\'"));
+                    break;
+
+                case StringKind.Command:
+                    if (sc.Parts.Count != 1)
+                        throw new FructoseCompileException("`` is escape-to-PHP operator, and only works with a string literal.", node);
+
+                    if (sc.Parts[0].NodeType != NodeTypes.StringLiteral)
+                        throw new FructoseCompileException("`` is escape-to-PHP operator, and only works with a string literal.", node);
+
+                    compiler.AppendLine("// BEGIN: escape-to-PHP");
+                    compiler.AppendLine(((StringLiteral)sc.Parts[0]).Value.ToString());
+                    compiler.AppendLine("// END: escape-to-PHP");
+                    break;
+
+                case StringKind.Mutable:
+                    if (sc.Parts.Count != 1 || sc.Parts[0].NodeType != NodeTypes.StringLiteral)
+                        throw new FructoseCompileException("String interpolation not supported _yet_", node);
+                    compiler.AppendLine("$_stack[] = F_String::__from_string('{0}');", ((StringLiteral)sc.Parts[0]).Value.ToString().Replace("'", "\\'"));
+                    break;
+            }
+        }
+    }
+}
