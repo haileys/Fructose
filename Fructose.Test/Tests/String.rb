e1059dc8d5afd9d391d7611f7c0d89b1e5a2c25a
Hailey Somerville <hailey@hailey.lol>
2011-01-22 23:43:13 +1100

    String test

---
+++ Fructose.Test/Tests/String.rb
@@ -0,0 +1,33 @@
+﻿#TEST EXPECTS:
+#ab
+#abc
+#aaa
+#true
+#b
+#Abc
+#0
+#abc
+#false
+#true
+#3
+#4
+#ABC
+#abcd
+
+puts "a" + "b"
+puts "a%sc" % "b"
+puts "a" * 3
+puts "a" == "a"
+puts "abc"[1]
+puts "abc".capitalize
+puts "Abc".casecmp "aBC"
+puts "ABC".downcase
+puts "hi".empty?
+puts "abc".include? "ab"
+puts "abc".length
+puts "3".to_n + 1
+puts "abc".upcase
+
+x = "a"
+x << "bc" << "d"
+puts x
