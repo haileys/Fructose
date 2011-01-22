a1f4a4dfe28af923cedc89b574110cd97203cfcc
Charlie Somerville <charlie@charliesomerville.com>
2011-01-22 19:28:52 +1100

    Number class in libfructose.php

--- libfructose/libfructose.php
+++ libfructose/libfructose.php
@@ -46,45 +46,140 @@ class F_Object
 	}
 }
 
-class F_Exception extends F_Object
+class F_Number extends F_Object
 {
-	public function __operator_eq($operand)
+	public static function __from_number($num)
 	{
-		if(get_class($operand) != 'F_Exception' && !is_subclass_of($operand, 'F_Exception'))
-			return new F_FalseClass;
-			
-		if($this->F_class() !== $operand->F_class())
-			return new F_FalseClass;
-			
-		return $this->message()->__operator_eq($operand->message());
-	}
-	public function F_backtrace()
-	{
-		return F_String::__from_string('');
-	}
-	public function F_message()
-	{
-		return $this->F_to_s();
-	}
-}
-
-class F_Fixnum extends F_Object
-{
-	public static __from_integer($int)
-	{
-		$obj = new F_Fixnum;
-		$obj->__INTEGER = $int;
+		$obj = new F_Number;
+		$obj->__NUMBER = $num;
 		return $obj;
 	}
-	
 	public function __operator_mod($operand)
 	{
-		
+		return F_Number::__from_number($this->__NUMBER % $operand->__NUMBER);
+	}
+	public function __operator_bitwiseand($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER & $operand->__NUMBER);
+	}
+	public function __operator_mul($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER * $operand->__NUMBER);
+	}
+	public function __operator_exp($operand)
+	{
+		return F_Number::__from_number(pow($this->__NUMBER, $operand->__NUMBER));
+	}
+	public function __operator_add($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER + $operand->__NUMBER);
+	}
+	public function __operator_sub($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER - $operand->__NUMBER);
+	}
+	public function __operator_unaryminus()
+	{
+		return F_Number::__from_number(-$this->__NUMBER);
+	}
+	public function __operator_div($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER / $operand->__NUMBER);
+	}
+	public function __operator_lt($operand)
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER < $operand->__NUMBER);
+	}
+	public function __operator_lshift($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER << $operand->__NUMBER);
+	}
+	public function __operator_lte($operand)
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER <= $operand->__NUMBER);
+	}
+	public function __operator_lte($operand)
+	{
+		if($this->__NUMBER < $operand->__NUMBER)
+			return F_Number::__from_number(-1);
+		if($this->__NUMBER > $operand->__NUMBER)
+			return F_Number::__from_number(-1);
+		return F_Number::__from_number(0);
+	}
+	public function __operator_eq($operand)
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER == $operand->__NUMBER);
+	}
+	public function __operator_stricteq($operand)
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER === $operand->__NUMBER);
+	}
+	public function __operator_gt($operand)
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER > $operand->__NUMBER);
+	}
+	public function __operator_rshift($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER >> $operand->__NUMBER);
+	}
+	public function __operator_gte($operand)
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER >= $operand->__NUMBER);
+	}
+	public function __operator_arrayget($index)
+	{
+		if($this->__NUMBER & (1 << $index->__NUMBER))
+			return F_Number::__from_number(1);
+		return F_Number::__from_number(0);
+	}
+	public function __operator_xor($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER ^ $operand->__NUMBER);
+	}
+	public function F_abs($operand)
+	{
+		return F_Number::__from_number(abs($this->__NUMBER));
+	}
+	public function F_even_QUES_()
+	{
+		return F_TrueClass::__from_bool(($this->__NUMBER % 2) == 0);
+	}
+	public function F_odd_QUES_()
+	{
+		return F_TrueClass::__from_bool(($this->__NUMBER % 2) == 1);
+	}
+	public function F_next()
+	{
+		return F_Number::__from_number($this->__NUMBER + 1);
+	}
+	public function F_to_s($base = 10)
+	{
+		return F_String::__from_string(base_convert((string)$this->__NUMBER, 10, $base));
+	}
+	public function F_zero_QUES_()
+	{
+		return F_TrueClass::__from_bool($this->__NUMBER == 0);
+	}
+	public function __operator_or($operand)
+	{
+		return F_Number::__from_number($this->__NUMBER | $operand->__NUMBER);
+	}
+	public function __operator_bitwisenot($operand)
+	{
+		return F_Number::__from_number(~$this->__NUMBER);
 	}
 }
 
 class F_TrueClass extends F_Object
 {
+	public static function __from_bool($bool)
+	{
+		if($bool)
+			return new F_TrueClass;
+		else
+			return new F_FalseClass;
+	}
+	
 	public function __operator_bitwiseand($operand)
 	{
 		$type = get_class($operand);
