8b40fb27fa56fa22d0f1bf699328d208bb658530
Hailey Somerville <hailey@hailey.lol>
2011-01-23 00:14:00 +1100

    Symbols

---
+++ Fructose.Test/Tests/Symbol.rb
@@ -0,0 +1,20 @@
+﻿#TEST EXPECTS:
+#1
+#true
+#foo
+#Foo
+#foo
+#FOO
+#false
+#3
+#o
+
+puts :foo <=> :bar
+puts :foo == :foo
+puts :foo
+puts :foo.capitalize
+puts :FoO.downcase
+puts :fOo.upcase
+puts :foo.empty?
+puts :foo.length
+puts :foo[2]
