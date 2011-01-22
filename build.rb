b485987030c6b267f65a7098324ff354354fd0c6
Charlie Somerville <charlie@charliesomerville.com>
2011-01-23 00:52:03 +1100

    Linux build script

---
+++ build.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+`rm -rf bin` if Dir.exist? "bin"
+["bin","bin/Fructose","bin/Fructose.Test","bin/Fructose.Test/Tests"].each { |dir| Dir.mkdir dir }
+
+`dmcs -out:bin/Fructose/Fructose.exe -target:exe -reference:lib/IronRuby.dll -reference:lib/Microsoft.Dynamic.dll -reference:lib/Microsoft.Scripting.dll #{Dir.glob("Fructose/**/*.cs").join " "}`
+`cp lib/* bin/Fructose/`
+
+`dmcs -out:bin/Fructose.Test/Fructose.Test.exe -target:exe -reference:bin/Fructose/Fructose.exe #{Dir.glob("Fructose.Test/**/*.cs").join " "}`
+`cp bin/Fructose/* bin/Fructose.Test/`
+`cp Fructose.Test/Tests/* bin/Fructose.Test/Tests`
+`cp libfructose/* bin/Fructose.Test`
+
+Dir.chdir "bin/Fructose.Test"
+system("mono Fructose.Test.exe")
+Dir.chdir "../.."
