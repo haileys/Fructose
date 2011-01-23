405b9b69a235c1b3c9ceeb56b5abedca23089153
Charlie Somerville <charlie@charliesomerville.com>
2011-01-23 14:16:11 +1100

    Fixed Literals and Strings

--- Fructose/Compiler/Generators/Literal.cs
+++ Fructose/Compiler/Generators/Literal.cs
@@ -11,6 +11,33 @@ namespace Fructose.Compiler.Generators
     {
         public override void Compile(Compiler compiler, Node node, NodeParent parent)
         {
+            if (((Literal)node).Value == null)
+            {
+                // nil
+                compiler.AppendLine("$_stack[] = new F_NilClass;");
+                return;
+            }
+            if (((Literal)node).Value is bool)
+            {
+                // True or False
+                compiler.AppendLine("$_stack[] = new F_{0}Class;", ((Literal)node).Value.ToString());
+                return;
+            }
+            if (((Literal)node).Value is string)
+            {
+                switch (((Literal)node).Value.ToString())
+                {
+                    case "self":
+                        compiler.AppendLine("$_stack[] = $this;", ((Literal)node).Value.ToString());
+                        break;
+                    case "__FILE__":
+                    case "__LINE__":
+                        compiler.AppendLine("$_stack[] = F_String::__from_string({0});", ((Literal)node).Value.ToString());
+                        break;
+                }
+                return;
+            }
+
             compiler.AppendLine("$_stack[] = F_Number::__from_number({0});", ((Literal)node).Value.ToString());
         }
     }
