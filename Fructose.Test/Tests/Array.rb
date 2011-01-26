293af8c9b7ca161172d76263d4e9c3ede6dc51a3
Hailey Somerville <hailey@hailey.lol>
2011-01-26 22:23:39 +1100

    Refactored Enumerable#all

--- Fructose.Test/Tests/Array.rb
+++ Fructose.Test/Tests/Array.rb
@@ -1,14 +1,17 @@
 ﻿#TEST EXPECTS:
 #true
 #false
+#3
+
 #false
 #true
 
 arr = [1, 2, 3]
 puts arr.all? { |o| o > 0 }
 puts arr.all? { |o| o > 1 }
+puts arr[2]
 
-arr = [false, false, false]
-puts arr.any?
-arr[1] = true
-puts arr.any?
+#arr = [false, false, false]
+#puts arr.any?
+#arr[1] = true
+#puts arr.any?
