abf59bd1057d20e5b6787506c5ffa223b0370939
Hailey Somerville <hailey@hailey.lol>
2011-01-26 22:28:51 +1100

    Reimplemented any?

--- Fructose.Test/Tests/Array.rb
+++ Fructose.Test/Tests/Array.rb
@@ -11,7 +11,7 @@ puts arr.all? { |o| o > 0 }
 puts arr.all? { |o| o > 1 }
 puts arr[2]
 
-#arr = [false, false, false]
-#puts arr.any?
-#arr[1] = true
-#puts arr.any?
+arr = [false, false, false]
+puts arr.any?
+arr[1] = true
+puts arr.any?
