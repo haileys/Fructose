f283ad9a780d7617856877d46f07845c8c0cabd4
Hailey Somerville <hailey@hailey.lol>
2011-01-28 22:03:03 +1100

    MD5 sum

---
+++ Fructose/Util.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Fructose
+{
+    public static class Util
+    {
+        public static string MD5(this string obj)
+        {
+            var digest = new MD5CryptoServiceProvider().ComputeHash(Encoding.UTF8.GetBytes(obj));
+            StringBuilder sb = new StringBuilder();
+            foreach (var b in digest)
+                sb.Append(b.ToString("x2"));
+            return sb.ToString();
+        }
+    }
+}
