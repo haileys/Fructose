22fb9aed7688f22ef489ae4dc524373fdb31403c
Charlie Somerville <charlie@charliesomerville.com>
2011-01-29 18:08:31 +1100

    Stepped up random iterations in test

--- Fructose.Test/Tests/Random.rb
+++ Fructose.Test/Tests/Random.rb
@@ -11,7 +11,7 @@ puts Random.seed 4
 
 # we'll repeat this a few times to make sure it passes
 n = 0
-while n < 100
+while n < 1000
   if Random.rand(10) > 10
     puts "FAIL: Random.rand(10) returned value over 10"
   end
@@ -19,7 +19,7 @@ while n < 100
 end
 
 n = 0
-while n < 100
+while n < 1000
   if Random.rand > 1.0
     puts "FAIL: Random.rand returned value over 1.0"
   end
