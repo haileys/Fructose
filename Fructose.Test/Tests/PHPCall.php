81e4d4f4da894064909d0d5eb463535ffd7eade9
Charlie Somerville <charlie@charliesomerville.com>
2011-02-01 20:41:34 +1100

    PHP interop

---
+++ Fructose.Test/Tests/PHPCall.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+function foo()
+{
+	return 42;
+}
+
+function fact($n)
+{
+	if($n <= 1)
+		return 1;
+	return $n * fact($n - 1);
+}
+
+class MyClass
+{
+    function foobar()
+	{
+		echo $this->msg . "\n";
+	}
+}
+function myClassFactory()
+{
+	return new MyClass;
+}
