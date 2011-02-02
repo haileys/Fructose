d4f431dbc2217712a84d4d0db05b93a534935cf7
Charlie Somerville <charlie@charliesomerville.com>
2011-02-02 21:05:28 +1100

    Misplaced bracket

--- libfructose/http.php
+++ libfructose/http.php
@@ -18,7 +18,7 @@ foreach(array("get" => $_GET, "post" => $_POST, "request" => $_REQUEST, "cookie"
 		{
 			$val = F_String::__from_string($v);
 		}
-		$pairs[] = F_Array::__from_array(array(F_Symbol::__from_string($k)), $val);
+		$pairs[] = F_Array::__from_array(array(F_Symbol::__from_string($k), $val));
 	}
 	$http_reqarrs[$sg] = F_Hash::__from_pairs($pairs);
 	F_Object::__add_global_method('F_' . $sg, create_function('', 'global $http_reqarrs; return $http_reqarrs["' . $sg . '"];'));
