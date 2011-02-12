d54b6f73c0c675472c17e880ad9cab072fe06b76
Hailey Somerville <hailey@hailey.lol>
2011-02-12 20:30:04 +1100

    Regexp test

---
+++ Fructose.Test/Tests/Regex.rb
@@ -0,0 +1,41 @@
+#TEST EXPECTS:
+#0
+#nil
+#nil
+#1
+#false
+#true
+#true
+#true
+#true
+#5
+#,
+#true
+#Hello
+#ello
+#1
+#6
+
+regex = /[a-z]{3}/
+
+puts regex === "hello"
+puts (regex =~ "h-el-lo").inspect
+
+puts /\d/.match("test").inspect
+puts /\d/.match("123").size
+
+puts /test/.casefold?
+puts /test/i.casefold?
+puts /test/xi.casefold?
+puts /test/ix.casefold?
+puts /test/mix.casefold?
+
+md = /^(([a-z])([a-z]+))(?<punc>[^a-z])/i.match "Hello, World!"
+puts md.size
+
+puts md[:punc]
+puts md[0].include? md[1]
+puts md[1]
+puts md[3]
+puts md.begin(3)
+puts md.end(:punc)
